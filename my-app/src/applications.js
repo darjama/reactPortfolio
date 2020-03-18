7c485176a785583be34f2fa1aae88af5bbf7e85c
Daryl Marco <darmarco@gmail.com>
2020-03-17 22:14:56 -0700

    Modified CSS for header and fixed typo on applications page#

--- my-app/src/applications.js
+++ my-app/src/applications.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from 'react';
+import './applications.css';
 
 const Applications = () => {
     return (
-      'Applications projects go here'
+      <div>
+        <p>
+        Here is a collection of software applications I've worked on:
+        </p>
+        <span className='hvr-float-shadow'>SofaMart</span>
+        <span className='hvr-float-shadow'>Craftworkery</span>
+      </div>
     )
 }
 
--- my-app/src/resume.js
+++ my-app/src/resume.js
@@ -4,8 +4,10 @@ import PDFViewer from 'pdf-viewer-reactjs'
 const Resume = () => {
     return (
         <div>
+          <div>
           <a href='./DarylMarcoResume.pdf' download='DarylMarcoResume.pdf' > <button>Download as PDF</button></a>
           <a href='./DarylMarcoResume.pdf.docx' download='DarylMarcoResume.docx' > <button> Download as Word Document</button></a>
+          </div>
         <PDFViewer
             document={{
                 url: './DarylMarcoResume.pdf',
