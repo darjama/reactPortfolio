5730da855443af548612cb41449527ca81d706ca
Daryl Marco <darmarco@gmail.com>
2020-04-16 21:42:33 -0700

    formatted contact buttons

--- my-app/src/About.js
+++ my-app/src/About.js
@@ -4,9 +4,17 @@ import './About.css'
 const About = () => {
   return (
     <div className='aboutMain'>
-    <span className='aboutTitle'>Daryl Marco</span><br/>
-    <span className='aboutTitle2'>Portfolio</span>
-    <p>Thank you for visiting my software development portfolio. I hope you like what you see.</p>
+      <br/>
+      <span className='aboutTitle'>Daryl Marco</span><br/>
+      <span className='aboutTitle2'>Portfolio</span>
+      <p>Thank you for visiting my software development portfolio. I hope you like what you see.</p>
+      <p>You can contact me via the following</p>
+      <div className='contactButtons'>
+        <span className='contactButton'>Email</span>
+        <span className='contactButton'>Phone</span>
+        <span className='contactButton'>LinkedIn</span>
+        <span className='contactButton'>GitHub</span>
+      </div>
     </div>
   )
 }
