6162da062229957334ea6fe2178a15cda06c4450
Daryl Marco <darmarco@gmail.com>
2020-04-18 22:42:49 -0700

    fixed resume centering

--- my-app/src/resume.js
+++ my-app/src/resume.js
@@ -9,14 +9,15 @@ const Resume = () => {
           <a href='./DarylMarcoResume.pdf' download='DarylMarcoResume.pdf' > <button>Download as PDF</button></a>
           <a href='./DarylMarcoResume.pdf.docx' download='DarylMarcoResume.docx' > <button> Download as Word Document</button></a>
           </div>
+          <div className='innerContainer'>
         <PDFViewer
             document={{
                 url: './DarylMarcoResume.pdf',
-
             }}
             scale={1.25}
             hideNavbar={true}
         />
+        </div>
         </div>
 
     )
