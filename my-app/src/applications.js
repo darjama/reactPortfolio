3332d9db2f5bb838fd3b5e175bf7efff07f34ad7
Daryl Marco <darmarco@gmail.com>
2020-06-16 10:32:08 -0700

    Added sections for application images and description text

--- my-app/src/applications.js
+++ my-app/src/applications.js
@@ -7,7 +7,29 @@ const Applications = () => {
         <p>
         Here is a collection of software applications I've worked on:
         </p>
-        <div className='appBody' background='./dmLogo.gif'>appBody</div>
+        <div className='appBody'>
+          <div className='appImage'>
+            <img src="./sofamart.gif" mode="fit" alt="sofamart animated GIF"/>
+          </div>
+          <div className='appText'><ul>
+            <li>Technologies Used:
+              <ul className="innerList" style={{columns: 2, margin: '0px 0px 0px 25px', listStyle: 'disc'}}>
+              <li>React</li>
+              <li>Styled Components</li>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>MySQL</li>
+              <li>Node</li>
+              <li>Webpack</li>
+              <li>AWS</li>
+            </ul>
+            </li>
+            <li>Coded interactive review application with summary, overview, filters, and helpfulness features</li>
+            <li>Successfully deployed application on AWS with a proxy for other page components</li>
+            <li>Improved load time by 65% by implementing text compression as measured by Google Page Insight</li>
+          </ul>
+          </div>
+        </div>
         <div className='appTitle'>SofaMart</div>
         <div className='appBody' background='./dmLogo.gif'>appBody</div>
         <div className='appTitle'>Craftworkery</div>
