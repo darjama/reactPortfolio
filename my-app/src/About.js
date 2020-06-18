a12bd8ebbced17d439759dcf906eca557dfa02d9
Daryl Marco <darmarco@gmail.com>
2020-06-17 23:58:19 -0700

    Changed About content and masthead formatting

--- my-app/src/About.js
+++ my-app/src/About.js
@@ -5,14 +5,13 @@ import { AiOutlineLinkedin } from 'react-icons/ai';
 const About = () => {
   return (
     <div className='aboutMain'>
-      <div className="masthead" role="img" ariaLabel="Coyote Hills Landscape">
+      <div className="masthead" role="img" aria-label="Coyote Hills Landscape">
         <h1 className="aboutTitle"> Daryl Marco </h1>
         <h2 className="aboutTitle2"> Portfolio </h2>
         <p className="mastText">
-          Thank you for visiting my software development portfolio. I hope you like what you see.
+          Thank you for visiting my software development portfolio. I am a software engineer specializing in front end and full stack web developement in the San Francsico Bay area.
           </p>
-          <br/>
-      <p className="mastText">You can contact me via:</p>
+      <p className="mastText">CONTACT LINKS</p>
       <div className='contactButtons'>
         <a href="mailto://darmarco@gmail.com" target="_blank" rel='noopener noreferrer'>
           <div className='contactButton'>
